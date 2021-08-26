074667cb618e796187bbf58ec4f20b051ed4916d
mohamedalaa1197 <62913078+mohamedalaa1197@users.noreply.github.com>
2021-08-26 18:06:05 +0200

    LinkedList Implementation

---
+++ LinkedList.js
@@ -0,0 +1,133 @@
+class XNode {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor(value) {
+        const newNode = new XNode(value);
+        this.head = newNode;
+        this.tail = this.head;
+        this.length = 1;
+    }
+
+    push(value) {
+        const newNode = new XNode(value);
+        if (!this.head) {
+            this.head = NewNode;
+            this.tail = NewNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+
+    pop() {
+        if (!this.head) return null;
+        let temp = this.head;
+        let pre = this.head;
+
+        while (temp.next) {
+            pre = temp;
+            temp = temp.next
+        }
+        this.tail = pre;
+        this.tail.next = null;
+        this.length--;
+        if (this.length === 0) {
+            this.head = this.tail = null;
+        }
+        return temp;
+    }
+
+    unshift(value) {
+        const newNode = new XNode(value);
+        if (!this.head) {
+            this.head = this.tail = newNode;
+        } else {
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length++;
+        return this;
+    }
+
+    shift() {
+        if (!this.head) return undefined;
+        let temp = this.head;
+        this.head = this.head.next;
+        temp.next = null;
+        this.length--;
+        if (this.length === 0) {
+            this.tail = null;
+        }
+        return temp;
+    }
+
+    get(index) {
+        if (index < 0 || index >= this.length) return undefined;
+        let temp = this.head;
+        for (let i = 0; i < index; i++) {
+            temp = temp.next;
+        }
+        return temp;
+    }
+
+    set(index, value) {
+        let temp = this.get(index);
+        if (temp) {
+            temp.value = value;
+            return true;
+        }
+        return false;
+    }
+
+    insert(index, value) {
+        if (index === 0) return this.unshift(value);
+        if (index === this.length) return this.push(value);
+        if (index < 0 || index >= this.length) return false;
+
+        const newNode = new XNode(value);
+        var temp = this.get(index - 1)
+        newNode.next = temp.next;
+        temp.next = newNode;
+        this.length++;
+        return true;
+    }
+    remove(index) {
+        if (index === 0) return this.shift(index);
+        if (index === this.length) return this.pop();
+        if (index < 0 || index >= this.length) return undefined;
+
+        var before = this.get(index - 1);
+        var temp = before.next;
+        before.next = temp.next;
+        temp.next = null;
+        this.length--;
+        return temp;
+    }
+
+    reverse() {
+        let temp = this.head;
+        this.head = this.tail;
+        this.tail = this.head;
+        let prev = null;
+        let next = temp.next;
+        for (let index = 0; index < this.length; index++) {
+            next = temp.next;
+            temp.next = prev;
+            prev = temp;
+            temp = next;
+        }
+        return this;
+    }
+}
+
+
+const linkedList = new LinkedList(4);
+linkedList.push(7);
+linkedList.push(5);
